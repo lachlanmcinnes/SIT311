6d38b38003f0a2dd95b6301ece52499b06899b5d
Lachlan McInnes <lrmcinne@deakin.edu.au>
2019-08-21 13:46:39 +1000

    NEW: React Weather App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import WeatherInfoComponent from'./WeatherInfoComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const fetch = require("node-fetch");
+const API_KEY = 'b3a1fe31fb871a134c029733070442ae';
+
+class App extends React.Component {
+	state = {
+		city : undefined,
+		temperature: undefined,
+		humidity: undefined,
+		pressure: undefined,
+		wind: undefined,
+		cloudiness: undefined
+	}
+	
+	getWeather = () =>
+	{
+		fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&units=metric&appid=${API_KEY}`)
+			.then(response => response.json())
+			.then(data => {
+				this.setState({
+					city:data.name,
+					temperature: data.main.temp,
+					humidity:data.main.humidity,
+					pressure:data.main.pressure,
+					wind:data.wind,
+					cloudiness:data.weather[0].description,
+				})
+			}
+		)
+	}
+	
+	handleChanges = (event) =>
+	{
+		this.setState({city:event.target.value})
+	}
+	
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<label for="city">City</label>
+					<input id={"City"}
+							type='text'
+							value={this.state.city}
+							onChange={this.handleChanges}/>
+					<input type="button"
+							onClick={this.getWeather}
+							value={'Get Weather'}/>
+				</header>
+				
+				<div className="App-content">
+					<WeatherInfoComponent 
+						city={this.state.city}
+						temperature={this.state.temperature}
+						humidity={this.state.humidity}
+						pressure={this.state.pressure}
+						wind={this.state.wind}
+						cloudiness={this.state.cloudiness}/>
+				</div>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
